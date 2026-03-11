e3119d0de85f4754a3ef3b8e32eef192aaa17789
erick peter <erickmwas10075@gmail.com>
2026-03-11 22:33:22 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,19 @@
+# question 4
+name="Ai stundent"
+print(f"Hello {name}")
+
+# question 5
+name="peter"
+age=20
+city="nairobi"
+print("name:",name)
+print("age:",age)
+print("city:",city)
+
+# question 6
+languages=["python","java","c++" ,"javascript" ,"Go"]
+print(languages [0])
+
+# question 7
+student={"name": "peter", "age": 20, "course": "AI"}
+print(student["name"])
